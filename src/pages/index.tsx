5b0086894d6fd9b5e9abcf2443145b2c97e282f1
daniellyncas <daniel.l@lyncas.net>
2021-02-25 01:23:35 -0300

    feature: adding icon to title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,20 @@ import CompletedChallenges from '../components/CompletedChallenges';
 import CountDown from '../components/CountDown';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
+//Component next
+import Head from 'next/head';
 // style
 import styles from '../styles/pages/Home.module.css';
+
+
 
 const Home = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
