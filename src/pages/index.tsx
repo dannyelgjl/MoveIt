fcf35814539f7251a39f58e277c926815f65b81a
daniellyncas <daniel.l@lyncas.net>
2021-02-25 05:12:51 -0300

    feature/ finishing challengeBox styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
+//Component next
+import Head from 'next/head';
 //Components
 import CompletedChallenges from '../components/CompletedChallenges';
 import CountDown from '../components/CountDown';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
-//Component next
-import Head from 'next/head';
+import ChallengeBox from '../components/ChallengeBox';
 // style
 import styles from '../styles/pages/Home.module.css';
-
 
 
 const Home = () => {
@@ -26,7 +26,7 @@ const Home = () => {
           <CountDown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
